299084866c77b14fc762ba574798a4a649eabb4c
caline <godfather20@qq.com>
2020-10-31 17:34:02 +0800

    方法引用学习

--- lambda-learning/src/main/java/com/caline/lambda/learning/App.java
+++ lambda-learning/src/main/java/com/caline/lambda/learning/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int n1 = 0;
+        System.out.println(n1==0);
+        
+        Integer n2 = 0;
+        System.out.println("0".equals(n2));
+        System.out.println("n2:"+n2.equals(0));
+        String n3 = "12";
+        System.out.println(n3.equals(12));
     }
 }
